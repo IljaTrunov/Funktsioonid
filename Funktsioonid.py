3e9fbeaa5186939598f7c1b0c90cf23eb0512b58
Opilane TTHK <opilane@tthk.ee>
2021-12-13 10:51:16 +0200

    Add project files.

---
+++ Funktsioonid.py
@@ -0,0 +1,19 @@
+from module1 import *
+
+#a=20.5
+#b=10.75
+#S=Pindala(a,b)
+#print(S)
+#1
+#z=4
+#y=2
+#z="/"
+#B=arithmetic(x,y,z)
+#print(B)
+#2
+#a=1945
+#D=is_year_leap(a)
+#print(D)
+#3
+#D=square(a)
+#print(D)
---
+++ module1.py
@@ -0,0 +1,59 @@
+#def Pindala(külg1:float,külg2:float)->float:
+ #   """Riistkülliku pindala leidmine 
+#    :param float külg1:Riistikülliku esimene külg
+#    :param float külg2:Riistikülliku teine külg
+#    :rtype:float
+#    """
+#    S=külg1*külg2
+ #   return S
+
+ #Praktiline töö
+ #1
+
+def arithmetic(x:int,y:int,z:str):
+	 """Saab teha +,-,*,/ ja tagastab arv, kui
+	 vastus on on arv ja "Tundmatu tehe", kui ei saa vastust leida
+	 :param float a: Esimene arv
+	 :param float b: Teine arv
+	 param str c: Aritmeetilise tehe
+	 :rtype:var
+	 """
+	 #if z=="+":
+		#B=x+y
+		#return B
+	 #elif z=="-":
+		#B=x-y
+		#return B
+	# elif z=="*":
+		#B=x*y
+		#return B
+	# elif z=="/":
+		#B=x/y
+		#return B
+	 #else:
+		 #pirnt("vale syntax")
+#2
+#def is_year_leap(a:int)->int:
+	#"""
+	#:param a: Aasta
+	# """
+	#if a%4==0:
+		#return True
+	#else:
+		# return False
+#3
+#def square(a:float):
+	# """
+	#:param float a:
+	# """
+	#S=a**2
+	#D=math.sqrt(2*S)
+	#P=S**4
+	#return S
+	#return D
+	#return P
+#4
+def aastaajad(a:int):
+	"""
+	:param int a: aastaajad
+	"""
