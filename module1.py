4ab0d3452aa7d1831db3e8753d114bd1d47ce810
IljaTrunov <93324206+IljaTrunov@users.noreply.github.com>
2021-12-15 11:52:16 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -53,7 +53,12 @@ def arithmetic(x:int,y:int,z:str):
 	#return D
 	#return P
 #4
-def aastaajad(a:int):
-	"""
-	:param int a: aastaajad
-	"""
+#def season(m:int):
+#    if m in (12,1,2):
+#        return "Зима"
+#    elif m in (3,4,5):
+#        return "Весна"
+#    elif m in (6,7,8):
+#        return "Лето"
+#    elif m in (9,10,11):
+#        return "Осень"
